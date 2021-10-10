f5f31a39e7df5a83d9510df8a4cad37b208f08b4
islamay <islamay.dean@gmail.com>
2021-10-10 18:27:29 +0800

    Merge from no-node

--- public/script.js
+++ public/script.js
@@ -1,4 +1,22 @@
 
+const widthRatio = 21
+const heightRatio = 29.7
+
+let n = 1
+let width = widthRatio
+let height = heightRatio
+
+while (width < window.innerWidth && height < window.innerHeight) {
+    width = widthRatio * n
+    height = heightRatio * n
+    n++
+}
+
+width = widthRatio * (n - 2)
+height = heightRatio * (n - 2)
+
 $('#flipbook').turn({
-    display: 'single'
+    display: 'single',
+    width,
+    height
 })
